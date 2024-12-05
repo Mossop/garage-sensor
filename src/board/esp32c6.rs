8c36028775bae5316598be3c1bfcd524df6698ff
Dave Townsend <dtownsend@oxymoronical.com>
2024-12-05 09:14:13 +0000

    Implement motion, temperature and humidity sensors

--- src/board/esp32c6.rs
+++ src/board/esp32c6.rs
@@ -1,5 +1,6 @@
 #[cfg(feature = "defmt")]
 use defmt::trace;
+use defmt::{error, warn};
 use embassy_executor::Spawner;
 use embassy_net::{Runner, Stack, StackResources};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
@@ -7,11 +8,13 @@ use embassy_time::Timer;
 use esp_alloc as _;
 use esp_hal::{
     clock::CpuClock,
-    gpio::GpioPin,
+    gpio::{AnyPin, GpioPin, Input, Pull},
+    i2c::master::{Config, I2c},
     ledc::{channel, timer, LSGlobalClkSource, Ledc, LowSpeed},
     peripherals::LEDC,
     prelude::*,
     rng::Rng,
+    Async,
 };
 use esp_wifi::{
     wifi::{
@@ -20,10 +23,40 @@ use esp_wifi::{
     },
     EspWifiController,
 };
+use mcutie::homeassistant::binary_sensor::BinarySensorState;
 use rand::RngCore;
 use static_cell::StaticCell;
 
+const HTU31D_ADDRESS: u8 = 0x40;
+const HTU32D_CMD_HEATER_OFF: u8 = 0x02;
+const HTU32D_CMD_RESET: u8 = 0x1E;
+const HTU32D_CMD_START_CONVERSION: u8 = 0x5e;
+const HTU32D_CMD_READ: u8 = 0x00;
+const HTU32D_CMD_READ_DIAGNOSTIC: u8 = 0x08;
+
 static LED_STATE: Signal<CriticalSectionRawMutex, bool> = Signal::new();
+static MOTION_STATE: Signal<CriticalSectionRawMutex, bool> = Signal::new();
+
+fn crc(value: u16) -> u8 {
+    let mut polynom: u32 = 0x988000; // x^8 + x^5 + x^4 + 1
+    let mut msb: u32 = 0x800000;
+    let mut mask: u32 = 0xFF8000;
+    let mut result: u32 = (value as u32) << 8; // Pad with zeros as specified in spec
+
+    while msb != 0x80 {
+        // Check if msb of current value is 1 and apply XOR mask
+        if (result & msb) != 0 {
+            result = ((result ^ polynom) & mask) | (result & !mask);
+        }
+
+        // Shift by one
+        msb >>= 1;
+        mask >>= 1;
+        polynom >>= 1;
+    }
+
+    (result & 0xff) as u8
+}
 
 #[embassy_executor::task]
 async fn led_task(ledc_peripheral: LEDC, gpio: GpioPin<15>) {
@@ -92,9 +125,130 @@ async fn net_task(mut runner: Runner<'static, WifiDevice<'static, WifiStaDevice>
     runner.run().await
 }
 
-#[derive(Clone, Copy)]
+#[embassy_executor::task]
+async fn motion_task(mut motion: Input<'static, AnyPin>) {
+    loop {
+        motion.wait_for_any_edge().await;
+
+        MOTION_STATE.signal(motion.is_high());
+    }
+}
+
+pub struct TempSensor {
+    i2c: I2c<'static, Async>,
+}
+
+impl TempSensor {
+    async fn new(mut i2c: I2c<'static, Async>) -> Self {
+        let _ = i2c.write(HTU31D_ADDRESS, &[HTU32D_CMD_RESET]).await;
+        let _ = i2c.write(HTU31D_ADDRESS, &[HTU32D_CMD_HEATER_OFF]).await;
+
+        let mut buffer = [0_u8; 2];
+        if i2c
+            .write_read(HTU31D_ADDRESS, &[HTU32D_CMD_READ_DIAGNOSTIC], &mut buffer)
+            .await
+            .is_ok()
+        {
+            let calculated_crc = crc(buffer[0] as u16);
+            if buffer[1] != calculated_crc {
+                warn!(
+                    "Invalid CRC for diagnostics data: {} != {}",
+                    calculated_crc, buffer[1]
+                );
+            } else if buffer[0] != 0 {
+                warn!("Diagnostics reports a fault: {}", buffer[0]);
+            }
+        }
+
+        Self { i2c }
+    }
+
+    pub async fn read(&mut self) -> Option<(f32, f32)> {
+        if let Err(e) = self
+            .i2c
+            .write(HTU31D_ADDRESS, &[HTU32D_CMD_START_CONVERSION])
+            .await
+        {
+            error!("Failed sending conversion start command: {}", e);
+            return None;
+        }
+
+        Timer::after_millis(50).await;
+
+        let mut buffer = [0_u8; 6];
+        if let Err(e) = self
+            .i2c
+            .write_read(HTU31D_ADDRESS, &[HTU32D_CMD_READ], &mut buffer)
+            .await
+        {
+            error!("Failed reading temperature and humidity: {}", e);
+            return None;
+        }
+
+        let temp = u16::from_be_bytes([buffer[0], buffer[1]]);
+        let temp_crc = buffer[2];
+        let humid = u16::from_be_bytes([buffer[3], buffer[4]]);
+        let humid_crc = buffer[5];
+
+        let humidity = (100.0 * humid as f32) / 65535.0;
+        let temperature = ((165.0 * temp as f32) / 65535.0) - 40.0;
+        trace!("Read temperature={}, humidity={}", temperature, humidity);
+
+        if crc(temp) != temp_crc || crc(humid) != humid_crc {
+            warn!(
+                "Invalid CRC for temperature or humidity: {} != {} || {} != {}",
+                crc(temp),
+                temp_crc,
+                crc(humid),
+                humid_crc
+            );
+            return None;
+        }
+
+        Some((temperature, humidity))
+    }
+}
+
+pub struct Led;
+
+impl Led {
+    pub async fn set(&self, state: bool) {
+        LED_STATE.signal(state);
+    }
+}
+
+pub struct Motion {
+    state: bool,
+}
+
+impl Motion {
+    pub fn state(&self) -> BinarySensorState {
+        self.state.into()
+    }
+
+    pub async fn wait(&mut self) -> BinarySensorState {
+        let current_state = self.state;
+        loop {
+            let mut new_state = MOTION_STATE.wait().await;
+            Timer::after_millis(50).await;
+
+            if let Some(st) = MOTION_STATE.try_take() {
+                new_state = st;
+            }
+
+            if new_state != current_state {
+                self.state = new_state;
+                return new_state.into();
+            }
+        }
+    }
+}
+
 pub struct Board {
-    stack: Stack<'static>,
+    pub network: Stack<'static>,
+    pub led: Led,
+    pub temp: TempSensor,
+    pub motion: Motion,
 }
 
 impl Board {
@@ -105,13 +259,12 @@ impl Board {
             config
         });
 
-        esp_alloc::heap_allocator!(72 * 1024);
+        esp_alloc::heap_allocator!(96 * 1024);
 
         let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
 
         let mut rng = Rng::new(peripherals.RNG);
 
-        trace!("1");
         static CONTROLLER: StaticCell<EspWifiController<'static>> = StaticCell::new();
         let esp_controller = CONTROLLER
             .uninit()
@@ -130,19 +283,37 @@ impl Board {
         static RESOURCES: StaticCell<StackResources<3>> = StaticCell::new();
         let resources = RESOURCES.init(StackResources::<3>::new());
 
-        let (stack, runner) = embassy_net::new(wifi_interface, config, resources, rng.next_u64());
+        let (network, runner) = embassy_net::new(wifi_interface, config, resources, rng.next_u64());
 
         spawner.spawn(connection(controller, ssid, password)).ok();
         spawner.spawn(net_task(runner)).ok();
 
-        Board { stack }
-    }
+        let i2c = I2c::new(
+            peripherals.I2C0,
+            Config {
+                frequency: 70.kHz(),
+                timeout: Some(10),
+            },
+        )
+        .with_sda(peripherals.GPIO22)
+        .with_scl(peripherals.GPIO23)
+        .into_async();
 
-    pub async fn set_led(&self, state: bool) {
-        LED_STATE.signal(state);
-    }
+        let temp = TempSensor::new(i2c).await;
 
-    pub async fn wait_for_network(&self) {
-        self.stack.wait_config_up().await;
+        let motion_input = Input::new(peripherals.GPIO17, Pull::Down);
+
+        let motion = Motion {
+            state: motion_input.is_high(),
+        };
+
+        spawner.spawn(motion_task(motion_input)).ok();
+
+        Board {
+            led: Led,
+            network,
+            temp,
+            motion,
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,152 @@
 #![no_std]
 
+use defmt::{debug, trace};
 use embassy_executor::Spawner;
-use embassy_time::Timer;
 
 #[cfg_attr(feature = "esp32c6", path = "board/esp32c6.rs")]
 mod board;
 
 use board::Board;
+use embassy_futures::select::select3;
+use embassy_time::Timer;
+use mcutie::{
+    homeassistant::{
+        binary_sensor::{BinarySensor, BinarySensorClass},
+        sensor::{Sensor, SensorClass, SensorStateClass},
+        AvailabilityState, AvailabilityTopics, Device, Entity, Origin,
+    },
+    McutieBuilder, McutieReceiver, McutieTask, MqttMessage, PublishBytes, Publishable, Topic,
+};
+
+use crate::board::{Led, Motion, TempSensor};
+
+const DEVICE_AVAILABILITY_TOPIC: Topic<&'static str> = Topic::Device("status");
+const MOTION_STATE_TOPIC: Topic<&'static str> = Topic::Device("motion/state");
+const TEMP_STATE_TOPIC: Topic<&'static str> = Topic::Device("temp/state");
+const HUMID_STATE_TOPIC: Topic<&'static str> = Topic::Device("humidity/state");
+
+const DEVICE: Device<'static> = Device::new();
+const ORIGIN: Origin<'static> = Origin::new();
+
+const MOTION_SENSOR: Entity<'static, 1, BinarySensor> = Entity {
+    device: DEVICE,
+    origin: ORIGIN,
+    object_id: "motion",
+    unique_id: Some("motion"),
+    name: "Motion",
+    availability: AvailabilityTopics::All([DEVICE_AVAILABILITY_TOPIC]),
+    state_topic: MOTION_STATE_TOPIC,
+    component: BinarySensor {
+        device_class: Some(BinarySensorClass::Motion),
+    },
+};
+
+const TEMPERATURE_SENSOR: Entity<'static, 1, Sensor> = Entity {
+    device: DEVICE,
+    origin: ORIGIN,
+    object_id: "temp",
+    unique_id: Some("temp"),
+    name: "Temperature",
+    availability: AvailabilityTopics::All([DEVICE_AVAILABILITY_TOPIC]),
+    state_topic: TEMP_STATE_TOPIC,
+    component: Sensor {
+        device_class: Some(SensorClass::Temperature),
+        state_class: Some(SensorStateClass::Measurement),
+        unit_of_measurement: Some("°C"),
+    },
+};
+
+const HUMIDITY_SENSOR: Entity<'static, 1, Sensor> = Entity {
+    device: DEVICE,
+    origin: ORIGIN,
+    object_id: "humidity",
+    unique_id: Some("humidity"),
+    name: "Relative Humidity",
+    availability: AvailabilityTopics::All([DEVICE_AVAILABILITY_TOPIC]),
+    state_topic: HUMID_STATE_TOPIC,
+    component: Sensor {
+        device_class: Some(SensorClass::Humidity),
+        state_class: Some(SensorStateClass::Measurement),
+        unit_of_measurement: Some("%"),
+    },
+};
+
+#[embassy_executor::task]
+async fn mqtt_task(
+    runner: McutieTask<
+        'static,
+        &'static str,
+        PublishBytes<'static, &'static str, AvailabilityState>,
+        0,
+    >,
+) {
+    runner.run().await;
+}
+
+async fn motion_sensor(mut motion: Motion) {
+    let _ = MOTION_SENSOR.publish_state(motion.state()).await;
+
+    loop {
+        let new_state = motion.wait().await;
+        debug!("Motion changed state");
+
+        let _ = MOTION_SENSOR.publish_state(new_state).await;
+    }
+}
+
+async fn temp_sensor(mut sensor: TempSensor) {
+    loop {
+        if let Some((temperature, humidity)) = sensor.read().await {
+            trace!(
+                "Read data, temperature={}°C, humidity={}%",
+                temperature,
+                humidity
+            );
+            let _ = TEMPERATURE_SENSOR.publish_state(temperature).await;
+            let _ = HUMIDITY_SENSOR.publish_state(humidity).await;
+        }
+
+        Timer::after_secs(30).await;
+    }
+}
+
+async fn discovery(receiver: McutieReceiver, led: Led) {
+    loop {
+        let message = receiver.receive().await;
+
+        match message {
+            MqttMessage::Connected | MqttMessage::HomeAssistantOnline => {
+                led.set(true).await;
+
+                let _ = DEVICE_AVAILABILITY_TOPIC
+                    .with_bytes(AvailabilityState::Online)
+                    .publish()
+                    .await;
+                let _ = MOTION_SENSOR.publish_discovery().await;
+                let _ = TEMPERATURE_SENSOR.publish_discovery().await;
+                let _ = HUMIDITY_SENSOR.publish_discovery().await;
+            }
+            MqttMessage::Disconnected => {
+                led.set(false).await;
+            }
+            _ => {}
+        }
+    }
+}
 
 pub async fn main(spawner: Spawner) {
     let board = Board::init(&spawner, env!("GARAGE_SSID"), env!("GARAGE_PASSWORD")).await;
-    board.wait_for_network().await;
+    let (receiver, mqtt_runner) =
+        McutieBuilder::new(board.network, "garage", env!("GARAGE_BROKER"))
+            .with_last_will(DEVICE_AVAILABILITY_TOPIC.with_bytes(AvailabilityState::Offline))
+            .build();
 
-    loop {
-        board.set_led(true).await;
-        Timer::after_millis(1000).await;
-        board.set_led(false).await;
-        Timer::after_millis(100).await;
-    }
+    spawner.spawn(mqtt_task(mqtt_runner)).unwrap();
+
+    select3(
+        discovery(receiver, board.led),
+        temp_sensor(board.temp),
+        motion_sensor(board.motion),
+    )
+    .await;
 }
